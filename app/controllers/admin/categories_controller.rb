2043c7e18abd7ff03898fb0c2d9a4a6a11966e1b
ericguthrie <ericguthrie12@gmail.com>
2021-04-29 19:32:54 +0000

    added first routes for category

---
+++ app/controllers/admin/categories_controller.rb
@@ -0,0 +1,40 @@
+class Admin::CategoriesController < ApplicationController
+
+  http_basic_authenticate_with name: 'jungle', password: 'book'
+
+
+  def index
+    @products = Product.order(id: :desc).all
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+
+    if @product.save
+      redirect_to [:admin, :products], notice: 'Product created!'
+    else
+      render :new
+    end
+  end
+
+
+  private
+
+  def product_params
+    params.require(:product).permit(
+      :name,
+      :description,
+      :category_id,
+      :quantity,
+      :image,
+      :price
+    )
+  end
+
+end
+
+
