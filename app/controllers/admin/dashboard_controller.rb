291f2816a7561de24449f3c86634ca4c24234c48
ericguthrie <ericguthrie12@gmail.com>
2021-04-29 06:17:03 +0000

    added about page

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,5 +1,8 @@
 class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: 'jungle', password: 'book'
+
   def show
+    @product = Product.order(id: :desc).all
   end
+
 end
