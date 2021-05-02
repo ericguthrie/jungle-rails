e2ac4f073881e6b0ad084e013fb5aeaf7bbc3660
ericguthrie <ericguthrie12@gmail.com>
2021-05-02 00:15:43 +0000

    added e-mail logic so no duplicate e-mails

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,24 @@ class User < ActiveRecord::Base
 
   has_secure_password
 
+  validates :name, presence: true
+  validates_uniqueness_of :email, case_sensitive: false, presence: true
+  validates :password, presence: true
+  validates :password_confirmation, presence: true
+  validates :password, length: { minimum: 3 }
+  
+  def self.authenticate_with_credentials(email, password)
+    downcase_email = email.downcase
+    user = User.find_by(email: downcase_email)
+    # puts user.inspect
+    # puts user.authenticate(password)
+    user && user.authenticate(password) # returns true or false
+    # if user && user.authenticate(password)
+    #   user
+    # else
+    #   nil
+    # end
+  end
+
 end
 
