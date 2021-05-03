f40558c02981c3548e80f124402e63b7211ad254
ericguthrie <ericguthrie12@gmail.com>
2021-05-03 21:12:59 +0000

    finished tests for admin product input

--- spec/models/product_spec_spec.rb
+++ spec/models/product_spec_spec.rb
@@ -14,5 +14,21 @@ RSpec.describe ProductSpec, type: :model do
       product = Product.create(name: nil, price: 12345, quantity: 1, category: category)
       expect(product.errors.full_messages).to include "Name can't be blank"
     end
+
+    it "doesn't save if price is missing" do
+      category = Category.create(name: "Urukhai")
+      product = Product.create(name:"leather shoes", price: nil, quantity: 1, category: category)
+      expect(product.errors.full_messages). to include "Price can't be blank"
+    end
+    it "doesn't save if quantity is missing" do
+      category = Category.create(name: "Urukhai")
+      product = Product.create(name:"leather shoes", price: 12345, quantity: nil, category: category)
+      expect(product.errors.full_messages). to include "Quantity can't be blank"
+    end
+    it "doesn't save if category is missing" do
+      category = Category.create(name: "Urukhai")
+      product = Product.create(name:"leather shoes", price: 12345, quantity: 1, category: nil)
+      expect(product.errors.full_messages). to include "Category can't be blank"
+    end
   end
 end
