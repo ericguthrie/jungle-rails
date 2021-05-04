2e7127d91971a0647e9bd54b39fc6e336c2edecc
ericguthrie <ericguthrie12@gmail.com>
2021-05-04 20:05:51 +0000

    added testing for products page

--- spec/features/home_page_spec.rb
+++ spec/features/home_page_spec.rb
@@ -1,19 +1,18 @@
 require 'rails_helper'
 
-RSpec.feature "Visitor navigates to home page", type: :feature, js: true do
+RSpec.feature "Visitor navigates from home page to product detail page", type: :feature, js: true do
 
-  # SETUP
   before :each do
     @category = Category.create! name: 'Apparel'
 
     10.times do |n|
       @category.products.create!(
-        name:  Faker::Hipster.sentence(3),
+        name: Faker::Hipster.sentence(3),
         description: Faker::Hipster.paragraph(4),
         image: open_asset('apparel1.jpg'),
         quantity: 10,
-        price: 64.99
-      )
+        price: 64.50
+        )
     end
   end
 
@@ -21,10 +20,8 @@ RSpec.feature "Visitor navigates to home page", type: :feature, js: true do
     # ACT
     visit root_path
 
-    # DEBUG
+    # DEBUG / VERIFY
     save_screenshot
-
-    # VERIFY
     expect(page).to have_css 'article.product', count: 10
   end
 end
---
+++ spec/features/product_details_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+RSpec.feature "ProductDetails", type: :feature do
+   # SETUP
+   before :each do
+    @category = Category.create! name: 'Apparel'
+
+    10.times do |n|
+      @category.products.create!(
+        name:  Faker::Hipster.sentence(3),
+        description: Faker::Hipster.paragraph(4),
+        image: open_asset('apparel1.jpg'),
+        quantity: 10,
+        price: 64.99
+      )
+    end
+  end
+
+  scenario "They see product details" do
+    # ACT
+    visit root_path
+    first('.product').click_link('Details')
+    # DEBUG /
+    expect(page).to have_css('.products-show')
+   
+  end
+end
